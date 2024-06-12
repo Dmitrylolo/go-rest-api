88a67712412ad3df783f6175b70ee004c00cb4ce
Dmitry Chernenkov <dmitrychee@gmail.com>
2024-06-12 12:14:04 +0300

    add middlewares

---
+++ internal/transport/http/middleware.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func JSONMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(log.Fields{
+			"method": r.Method,
+			"path":   r.URL.Path,
+		}).Info("Request received")
+		next.ServeHTTP(w, r)
+	})
+}
